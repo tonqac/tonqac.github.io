97e2b51b1bff304171acb26e66ef52e1a74d6a29
Gastón Giménez <44003590+tonqac@users.noreply.github.com>
2022-10-24 11:36:45 -0300

    Add files via upload
    Prueba gitpages

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", ()=>{
+    console.log("SW","Install")
+});
+
+self.addEventListener("activate", ()=>{
+    console.log("SW","Activate")
+});
+
+self.addEventListener("fetch", ()=>{
+    console.log("SW","Fetch")
+});
